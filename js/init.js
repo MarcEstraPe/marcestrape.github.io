0ca9d7b09f39e2982d78e3ccedcf45eb57b9f646
Marc Estrada <murks22@gmail.com>
2014-11-23 21:32:08 +0100

    Added 'route' to pet

--- js/init.js
+++ js/init.js
@@ -10,10 +10,10 @@
 		reset: 'full',
 		ignoreOrientation: true,
 		breakpoints: {
-			'global':	{ range: '*', href: 'css/style.css' },
-			'desktop':	{ range: '481-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
-			'1000px':	{ range: '481-1200', href: 'css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewportWidth: 1080 },
-			'mobile':	{ range: '-480', href: 'css/style-mobile.css', containers: '100%', grid: { collapse: true, gutters: 10 }, lockViewport: true }
+			'global':	{ range: '*', href: '/css/style.css' },
+			'desktop':	{ range: '481-', href: '/css/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
+			'1000px':	{ range: '481-1200', href: '/css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewportWidth: 1080 },
+			'mobile':	{ range: '-480', href: '/css/style-mobile.css', containers: '100%', grid: { collapse: true, gutters: 10 }, lockViewport: true }
 		}
 	});
 
