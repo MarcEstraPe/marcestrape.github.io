084e1647034c2bf18c33dc553367d41dfcb3c01a
Marc Estrada <murks22@gmail.com>
2015-01-13 20:52:55 +0100

    Improved spam bot filter and added new one

---
+++ js/block_spam_bots.js
@@ -0,0 +1,22 @@
+var blocklink = [
+    'http://darodar.com',
+    'http://econom.co',
+    'http://ilovevitaly.com',
+    'http://ilovevitaly.co',
+    'http://priceg.com',
+    'http://blackhatworth.com'
+];
+
+function checkSpamBots() {
+    console.log("Checking spam bots for referrer '".concat(document.referrer).concat("'"));
+
+    for (var b = 0; b < blocklink.length; b++) {
+        console.log("Checking match with ".concat(blocklink[b]));
+        if (document.referrer.match(blocklink[b])) {
+            window.location = "http://google.com/";
+            console.log(blocklink[b].concat(" detected as spam bot. Block acces!"));
+            return;
+        }
+    }
+    console.log("No spam bot as referrer found!");
+}
